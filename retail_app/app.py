b4a4fafc48c92830efeb371dd90f9e3f50ff95f6
Kaua Onorato <kauaonorato.dev@gmail.com>
2025-05-28 06:21:46 -0400

    feat: Adiciona aleatoriedade na atualização nos status dos pedidos gerados

--- retail_app/app.py
+++ retail_app/app.py
@@ -2,8 +2,9 @@ import os
 import psycopg2
 from faker import Faker
 from dotenv import load_dotenv
-from datetime import datetime
+from datetime import datetime, timedelta
 from time import sleep
+from random import randint, gauss
 
 load_dotenv()
 
@@ -53,27 +54,40 @@ def insert_orders(cursor):
     order_id = cursor.fetchone()[0]
     print(f"Order ID: {order_id}")
     
-    return order_id
+    return order_id, order_time
 
-def insert_orders_histories(cursor, order_id):
+def insert_orders_histories(cursor, order_id, initial_order_time):
     order_history_items_query = """
         INSERT INTO ordershistory (order_id, status, updated_at)
         VALUES (%s, %s, %s)
         RETURNING history_id
     """
-    
-    status_elements = (
-        'new',
-        'intransit',
-        'delivered'
-    )
-    
-    status = fk.random_element(elements=status_elements)
-    updated_at = datetime.now()
-    
-    cursor.execute(order_history_items_query, (order_id, status, updated_at))
-    history_id = cursor.fetchone()[0]
-    print(f"Order History ID: {history_id}")
+     
+    # 1. Inserir status 'new'
+    cursor.execute(order_history_items_query, (order_id, 'new', initial_order_time))
+    print(f"Order History ID: {cursor.fetchone()[0]} - Status: new")
+
+    current_time = initial_order_time 
+    minutes_time = randint(0, 59)
+    hours_time = randint(0, 23)
+
+    # 2. 75% chance de inserir status 'intransit'
+    if randint(0, 3): 
+        delay_days_intransit = gauss(5, 3)
+        delay_days_intransit = max(1, delay_days_intransit)
+        
+        current_time += timedelta(days=delay_days_intransit, hours=hours_time, minutes=minutes_time)
+        cursor.execute(order_history_items_query, (order_id, 'intransit', current_time))
+        print(f"Order History ID: {cursor.fetchone()[0]} - Status: intransit")
+
+        # 3. 50% chance de inserir status 'delivered' após o status 'intransit'	
+        if randint(0, 1):
+            delay_days_delivered = gauss(10, 5)
+            delay_days_delivered = max(1, delay_days_delivered)
+            
+            current_time += timedelta(days=delay_days_delivered, hours=hours_time, minutes=minutes_time)
+            cursor.execute(order_history_items_query, (order_id, 'delivered', current_time))
+            print(f"Order History ID: {cursor.fetchone()[0]} - Status: delivered")
     
 try:
     print('Inserting data...')
@@ -82,8 +96,8 @@ try:
         
     for _ in range(1000):
         conn, cursor = connect_db()
-        order_id = insert_orders(cursor)
-        insert_orders_histories(cursor, order_id)
+        order_id, order_time = insert_orders(cursor)
+        insert_orders_histories(cursor, order_id, order_time)
         print('-' * 50)
         
         conn.commit()
